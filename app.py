7706cc97ea81f79ebc3c2a0bd38f8c37b20b2b0b
TarekMebrouk <tarekmebroukk@gmail.com>
2022-01-21 17:55:19 +0100

    Update: add parser principal function - Add configuration for chebi SDF file path inside JSON config file - Add run method to parse, extract and save molecules

--- app.py
+++ app.py
@@ -1,17 +1,5 @@
-from models.molecule import *
 from models.parser import *
-from services.database import *
 
 if __name__ == '__main__':
     parser = Parser()
-
-    # extract molecule information
-    molecule = Molecule(args='CHEBI:90')
-    molecule.display()
-
-    # clear all rows in tables
-    database = Database()
-    database.clear_tables()
-
-    # save molecule
-    database.save_molecule(molecule)
+    parser.run()
--- models/parser.py
+++ models/parser.py
@@ -1,13 +1,15 @@
+from models.molecule import *
+from services.database import *
+import json
+
 
 class Parser:
 
     def __init__(self):
-        # initialize molecules file path
-        self.source_path = '../ChEBI_complete_3star.sdf'
-
-        # initialize molecules IDs
-        self.molecules_ID = []
-        self.debug_level = 0
+        # initialize molecules SDF file path from JSON config file
+        config_file = open('config.json')
+        configuration = json.load(config_file)
+        self.source_path = configuration['chebi']['path']
 
     # open & read SDF file from local storage file 'chEBI_complete_3star.sdf'
     def read_sdf_file(self):
@@ -19,17 +21,39 @@ class Parser:
     # read & extract all chEBI molecules IDs
     def extract_molecules_ID(self):
         # read SDF file & extract all information inside buffer 'data'
+        molecules_id = []
         data = self.read_sdf_file()
         index = 0
 
         # extract all molecules ID from buffer 'data'
         for line in data:
             if index == 1:
-                self.molecules_ID.append(line.strip())
+                molecules_id.append(line.strip())
                 index = 0
                 continue
             if "<ID>" in line or "<ChEBI ID>" in line:
                 index = 1
                 continue
-        if self.debug_level >= 5:
-            print(self.molecules_ID)
+        return molecules_id
+
+    # principal function of parsing
+    def run(self):
+
+        # extract molecules IDs
+        molecules_id = self.extract_molecules_ID()
+
+        # extract information of each ChEBI ID
+        counter = 0
+        database_service = Database()
+
+        for id_chebi in molecules_id:
+
+            # display parsing state
+            counter += 1
+            print(f'Molecule N°{counter}')
+
+            # extract information & parse molecular structure
+            molecule = Molecule(args=id_chebi)
+
+            # save molecule in SQL database
+            database_service.save_molecule(molecule)
